d31229b3d814144ffd150ace9f1ca42dc8ba7a7f
ramyapop <ramyamgcworld@gmail.com>
2023-04-29 12:16:58 +0530

    done

---
+++ Do.java
@@ -0,0 +1,25 @@
+package looping;
+
+public class Do {
+public static void main(String[] args) {//exit level loop
+
+		
+		int a=50;
+		
+		do {
+			
+			
+			System.out.println(a);
+			
+			a--;
+			
+			
+			
+			
+			
+		} while (a>=40);
+		
+		
+	}
+}
+
